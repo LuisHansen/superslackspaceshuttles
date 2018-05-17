865256cd372164bf9b1c93d4548eded808cb4dbe
Luis Henrique das Neves Hansen <luis.hansen@certsys.local>
2018-05-17 10:39:27 -0300

    Testes de classe em C++

---
+++ Source/SnS/SnSGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SnSGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SNS_API ASnSGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
